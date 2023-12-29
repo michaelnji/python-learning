66dbcc6bc27beac943d41c39f7ca602e4d808f75
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-12-29 21:16:43 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    choices = {"player":player_choice,"computer":computer_choice}
+    return choices
+
+choices = get_choices()
+print(choices)
+
