2f6d7cce7110982eb35cf7d1c62246912706adfc
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-12-29 23:35:38 +0100

    Refactor game logic and add user input for choices
    Consolidate game logic into separate functions, implement user input for
    player choices, and print game result. This enhances user interaction
    and clarity of game outcomes. No associated issues.

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
+import random
+
+
 def get_choices():
-    player_choice = "rock"
-    computer_choice = "paper"
-    choices = {"player":player_choice,"computer":computer_choice}
+    player_choice = input("Enter a choice (rock, paper, scissors)")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
     return choices
 
-choices = get_choices()
-print(choices)
 
+def check_win(player, computer):
+    print(f"You chose {player} and the computer chose {computer}")
+    if player == computer:
+        return "it's a tie!"
+    elif player == "rock" and computer == "scissors":
+        return "rock smashes scissors! you win"
+    elif player == "rock" and computer == "paper":
+        return "paper covers rock! computer win"
+
+check_win('rock','paper')
